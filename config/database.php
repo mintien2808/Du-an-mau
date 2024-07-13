2872c5088e7bf476167bc62607b6ba94bda5ec34
mt280_pie <tienpmpk03580@fpt.edu.vn>
2024-07-13 09:38:44 +0700

    folder

---
+++ config/database.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+function db_connect()
+{
+    try {
+        $conn = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8mb4', DB_USER, DB_PASS);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $conn;
+    } catch (PDOException $e) {
+        die("Connection failed: " . $e->getMessage());
+    }
+}
+?>
